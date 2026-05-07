8af8de9de1312de201354d611dc06e5cae048da9
Aleksandre Tcharbadze <aleksandre.tcharbadze@pipeline.gmbh>
2026-05-07 12:55:17 +0200

    {commit_message2}3

--- app/layout.js
+++ app/layout.js
@@ -41,7 +41,7 @@ export const metadata = {
     description:
       "ხელნაკეთი დიზაინერული მინის ავეჯი და ინტერიერის ობიექტები.",
 
-    images: ["/og-image1.jpg"],
+    images: ["/og-image.jpg"],
 
     type: "website",
   },
